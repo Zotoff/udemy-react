e9faacc100ebd34d4e6f58975528707d30410448
Павел Зотов <paulzotoff@yandex.ru>
2019-05-25 15:19:40 +0300

    React transitions

--- udemy-react-transitions/src/components/Transition.js
+++ udemy-react-transitions/src/components/Transition.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import Transition from 'react-transition-group/Transition';
 import '../css/App.css';
 
 class TransitionComp extends Component{
 
+    state = {
+        show: false
+    }
+    showDiv = () => {
+        this.setState({
+            show: !this.state.show ? true:false
+        })
+    }
+
     render(){
         return(
             <div>
-                Transition
+            <Transition
+                in={this.state.show}
+                timeout={2000}
+            >
+              {state => 
+                <div className={`square square-${state}`}>
+                    {`square square-${state}`}
+              </div>
+              }  
+
+            </Transition>
+            <div className="showDiv" onClick={this.showDiv}>Show or hide</div>
             </div>
 
         )

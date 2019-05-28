04e3199acddef0529a205257adecf060c0608f51
Павел Зотов <paulzotoff@gmail.com>
2019-05-28 23:53:48 +0300

    Working with news list

--- nba-app/src/hoc/home/home.js
+++ nba-app/src/hoc/home/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import NewsSlider from '../../components/widgets/NewsSlider/slider'
+import NewsSlider from '../../components/widgets/NewsSlider/slider';
+import NewsList from '../../components/widgets/NewsList/newslist';
 
 const Home = () => {
     return (
@@ -11,6 +12,12 @@ const Home = () => {
                 settings={{
                     dots: false
                 }}
+            />
+            <NewsList 
+                type="card"
+                loadmore={true}
+                start={3}
+                amount={3}
             />
         </div>
     )

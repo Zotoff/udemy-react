8070f60452f78ae3f1506c4af1e2d863e5d5bdad
Павел Зотов <paulzotoff@gmail.com>
2019-06-01 22:40:24 +0300

    News Main View

--- nba-app/src/routes.js
+++ nba-app/src/routes.js
@@ -5,6 +5,7 @@ import Home from './hoc/home/home';
 import Layout from './hoc/layout/layout';
 import NewsArticle from './components/Articles/News/Post/index';
 import VideoArticle from './components/Articles/Videos/Video/index';
+import NewsMain from './components/Articles/News/NewsMain/newsMain';
 
 class Routes extends Component {
     render(){
@@ -13,6 +14,7 @@ class Routes extends Component {
             <Layout>
                 <Switch>
                     <Route path="/" exact component={Home} />
+                    <Route path="/news" exact component={NewsMain} />
                     <Route path="/articles/:id" exact component={NewsArticle} />
                     <Route path="/videos/:id" exact component={VideoArticle}/>
                 </Switch>

bd5a20e30ae316f5db85064d03c3b46b4321cb04
Павел Зотов <paulzotoff@gmail.com>
2019-06-05 22:40:22 +0300

    Forms template finished

--- udemy-forms/src/widgets/Forms/formFields.js
+++ udemy-forms/src/widgets/Forms/formFields.js
@@ -27,11 +27,63 @@ const FormFields = (props) => {
         : null
     }
 
-    const changeHandler = (event, id) => {
+    // Working with changes
+    const changeHandler = (event, id, blur) => {
+
+        // Grabs the data from props
         const newState = props.formData;
         newState[id].value = event.target.value;
 
+        // Running the validation when focused
+        if(blur) {
+            // Running checks on an input, newState[id] - which input we check
+            let validData = validate(newState[id]);
+
+            newState[id].valid = validData[0];
+            newState[id].validationMessage = validData[1];
+        }
+
+        // Working with focus
+        newState[id].touched = true;
+        // Changing the state
         props.change(newState);
+    }
+
+    // Receives data, checks the rules
+    const validate = (element) => {
+        let error = [true, ''];
+
+        // Checking the minLen validation property
+        if(element.validation.minLen) {
+            // Checking the length of element input
+            const valid = element.value.length >= element.validation.minLen;
+            const message = `${!valid ? 'min length is ' + element.validation.minLen : ''}`
+
+            error = !valid ? [valid, message] : error
+        }
+
+        // Checking the required validation property
+        if(element.validation.required){
+            const valid = element.value.trim() !== ''; //Trimming the whitespaces
+            const message = `${!valid ? 'this field is required' : ''}`
+
+            error = !valid ? [valid, message] : error
+        }
+
+        return error;
+    }
+
+    // Shows the validation message in the template
+    const showValidation = (data) => {
+        let errorMessage = null;
+        if(data.validation && !data.valid) {
+            errorMessage = (
+                <div className="label_error">
+                    {data.validationMessage}
+                </div>
+            )
+        }
+        return errorMessage;
     }
 
     const renderTemplates = (data) => {
@@ -46,10 +98,51 @@ const FormFields = (props) => {
                         <input 
                             {...values.config}
                             value={values.value}
+                            onBlur={
+                                (event) => changeHandler(event, data.id, true) //true - for working with blur prop
+                            }
+                            onChange={
+                                (event) => changeHandler(event, data.id, false)
+                            }
+                        />
+                        {showValidation(values)}
+                    </div>
+                )
+            break;
+            case('textarea'):
+                formTemplate = (
+                    <div>
+                        {showLabel(values.label, values.labelText)}
+                        <textarea
+                            {...values.config}
+                            value={values.value}
                             onChange={
                                 (event) => changeHandler(event, data.id)
                             }
-                        />
+                        ></textarea>
+                    </div>
+                )
+            break;
+            case('select'):
+                formTemplate = (
+                    <div>
+                        {showLabel(values.label, values.labelText)}
+                        <select
+                            value={values.value}
+                            name={values.config.name}
+                            onChange={
+                                (event) => changeHandler(event, data.id)
+                            }
+                        >
+                            {values.config.options.map((item, i) => {
+                             return (
+                                <option
+                                    key={i}
+                                    value={item.val}
+                                >{item.text}</option>
+                             )
+                            })}
+                        </select>
                     </div>
                 )
             break;

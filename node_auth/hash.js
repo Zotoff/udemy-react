fb4e56a71bf3ac4abfb74ce770276247c4280779
Павел Зотов <paulzotoff@gmail.com>
2019-07-11 22:26:18 +0300

    Trying to finish the authorisation

--- node_auth/hash.js
+++ node_auth/hash.js
@@ -1,11 +1,35 @@
-const bcrypt = require('bcrypt');
+const brcypt = require('bcrypt');
+const {MD5} = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
-// Generating a sault and hashing the password
-bcrypt.genSalt(10,(err,salt)=>{
-    if(err) return next(err);
+// const secret = 'mysecretpassword';
+// const secretSalt = 'asdasdasdas';
 
-    bcrypt.hash('password123',salt,(err,hash)=>{
-        if(err) return next(err);
-        console.log(hash);
-    })
-})
+
+// const user = {
+//     id: 1,
+//     token: MD5('dasdasda').toString() + secretSalt
+// }
+
+// const receivedToken = 'ac6555bfe23f5fe7e98fdcc0cd5f2451'
+
+// if(user.token === receivedToken){
+//     console.log('move forward')
+// }else{
+//     console.log("Doesn't match")
+// }
+
+
+// console.log(user);
+
+/*Working with JSON Web Token*/
+
+const id = '1000';
+const secret = 'supersecret';
+
+const receivedToken = 'eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y';
+
+const token = jwt.sign(id,secret);
+const decodeToken = jwt.verify(receivedToken,secret); 
+
+console.log(decodeToken)
--- node_auth/server/server.js
+++ node_auth/server/server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const SALT = 10;
+
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Working with BCRYPT
-const bcrypt = require('bcrypt');
-const SALT = 10;
+
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 /*User info*/
 
+/*Creating the Schema for Mongoose*/
 const userSchema = mongoose.Schema({
     email: {
         type: String,
@@ -22,38 +28,78 @@ const userSchema = mongoose.Schema({
         type: String,
         required: true,
         minlength: 6
+    },
+    token:{
+        type: String
     }
 })
 
 // Running something before making a request. Method Next() is called to make the app going to the next step as it is the Middleware
 userSchema.pre('save', function(next){
+
+    // Declaring the user
     var user = this;
 
     // Checking if the password is modified
 
     if(user.isModified('password')) {
+
+        // Generating the salt
         bcrypt.genSalt(SALT, function(err,salt){
             if(err) return next(err);
-            bcrypt.hash(user.password,salt, function(err, hash){
+            // Hashing
+            bcrypt.hash(user.password,salt,function(err, hash){
                 if(err) return next(err);
                 user.password = hash;
                 next();
-            });
+            })
         })
     } else {
         next();
     }
-
-    
 })
 
-const User = mongoose.model('User',userSchema)
-/*END User info*/
+// Method for comparing the password
+userSchema.methods.comparePassword = function(candidatePassword,cb){
+    bcrypt.compare(candidatePassword,this.password,function(err,isMatch){
+        if(err) throw cb(err);
+        cb(null,isMatch)
+    })
+}
+// Method which generates the token
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    // Generating the token with user id
+    var token = jwt.sign(user._id.toHexString(), 'supersecret');
+
+    user.token = token;
+    user.save(function(err,user){
+        // If error returns the callback function for error
+        if(err) return cb(err)
+        cb(null,user)
+    })
+}
+
+// Grabbing the token and verifying the correctness
+userSchema.statics.findByToken = function(token, cb){
+    const user = this;
+    
+    // Verifies the token
+    jwt.verify(token, 'supersecret', function(err,decode){
+        user.findOne({"_id":decode,"token":token},function(err,user){
+            if(err) return cb(err)
+            cb(null,user)
+        })
+    })
+
+}
+
+const User = mongoose.model('User',userSchema); // Specifies the collection
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/auth')
 
-app.use(bodyParser.json());
+
 
 // Creating a route
 app.post('/api/user',(req, res)=>{
@@ -66,6 +112,36 @@ app.post('/api/user',(req, res)=>{
         res.status(200).send(doc)
     })
 });
+
+
+/*User login*/
+app.post('/api/user/login',(req, res)=>{
+    User.findOne({'email':req.body.email},(err,user)=>{
+        if(!user) res.json({message: "Auth failed, user not found"})
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(err) throw err;
+            if(!isMatch) return res.status(400).json({
+                message: "Wrong password"
+            });
+            user.generateToken((err,user)=>{
+                if(err) return res.status(400).send(err);
+                res.cookie('auth',user.token).send('ok')
+            });
+        })
+    })
+})
+
+// Creates a route for profile
+app.get('user/profile',(req,res)=>{
+    let token = req.cookies.auth;
+
+    User.findByTolen(token,(err,user)=>{
+        if(err) throw err;
+        if(!user) return res/status(401).send('no access');
+        res.status(200).send('you have access');
+    })
+})
+
 
 app.listen(port, ()=>{
     console.log(`Started on port ${port}`)
